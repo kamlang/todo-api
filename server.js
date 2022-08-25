cac19f673ed98a3fe95b59e1f51959a09e216bc8
Albin Chatton <714006+kamlang@users.noreply.github.com>
2022-08-25 10:16:18 +0200

    set allowed origin as env variable

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(jwkcheck)
 app.use(express.json())
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://192.168.1.6:3000");
+  res.header("Access-Control-Allow-Origin", process.env.ALLOWED_ORIGIN);
   res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
   next();
